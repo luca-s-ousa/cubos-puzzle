53acb822c7c75eea7acae1463df51ac2c5b147d9
Lucas Sousa <lsousatop5771@gmail.com>
2023-06-07 20:07:25 -0300

    added data

---
+++ src/data/Cards.ts
@@ -0,0 +1,87 @@
+import { Letter } from "../types/Letter";
+
+import NotTurnedImg from "../assets/notTurned.svg";
+
+import Heroku from "../assets/heroku.svg";
+import Nodejs from "../assets/nodejs.svg";
+import ReactImg from "../assets/react.svg";
+import Netlify from "../assets/netlify.svg";
+import HtmlCss from "../assets/htmlcss.svg";
+import Javascript from "../assets/javascript.svg";
+
+const letters: Letter[] = [
+  {
+    id: 1,
+    backImg: Heroku,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 2,
+    backImg: Heroku,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 3,
+    backImg: Nodejs,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 4,
+    backImg: Nodejs,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 5,
+    backImg: ReactImg,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 6,
+    backImg: ReactImg,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 7,
+    backImg: Netlify,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 8,
+    backImg: Netlify,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 9,
+    backImg: HtmlCss,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 10,
+    backImg: HtmlCss,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 11,
+    backImg: Javascript,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+  {
+    id: 12,
+    backImg: Javascript,
+    notTurnedImg: NotTurnedImg,
+    turn: false,
+  },
+];
+
+export default letters;
