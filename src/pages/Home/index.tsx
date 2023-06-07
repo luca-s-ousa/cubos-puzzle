f63513fc6b49ac588c01aabfce6a97f8253cc6fe
Lucas Sousa <lsousatop5771@gmail.com>
2023-06-07 20:31:57 -0300

    change styles Home and import Letters

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import Aside from "../../components/Aside";
+import Letters from "../../components/Letters";
 import "./styles.css";
 
 function Home() {
   return (
     <div className="container__app">
       <Aside />
+      <div className="content__main">
+        <Letters />
+      </div>
     </div>
   );
 }
